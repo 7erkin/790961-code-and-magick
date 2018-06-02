c68aadc0632dca2eff08c1cd2a05f951e57014ad
7erkin <olegsemerkin777@gmail.com>
2018-06-02 11:49:11 +0300

    Add cloud rendering

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,9 @@
+var CLOUD_WIDTH = 270;
+var CLOUD_HEIGHT = 420;
+var SHIFT_SHADOW = 10;
+var X_COOR_CLOUD = 100;
+var Y_COOR_CLOUD = 0;
+
 getMaxValue = function(array){
     var temp = array[0]; 
     for(var i = 1; i < array.length; ++i)
@@ -5,6 +11,17 @@ getMaxValue = function(array){
             temp = array[i];
     return temp;
 }
+
+renderCloud = function(renderParameters){
+    renderParameters.context.fillStyle = renderParameters.color;
+    renderParameters.context.fillRect(renderParameters.x, renderParameters.y, renderParameters.height, renderParameters.width);
+}
+
 window.renderStatistics = function(ctx, names, times){
     var maxValue = getMaxValue(times);
+    var renderCloudParameters = {context: ctx, x: X_COOR_CLOUD, y: Y_COOR_CLOUD, width: CLOUD_WIDTH, height: CLOUD_HEIGHT, color: 'white'};
+    var renderShadowCloudParameters = {context: ctx, x: X_COOR_CLOUD + SHIFT_SHADOW, y: Y_COOR_CLOUD + SHIFT_SHADOW, width: CLOUD_WIDTH, height: CLOUD_HEIGHT,
+         color: 'black'};
+    renderCloud(renderShadowCloudParameters);         
+    renderCloud(renderCloudParameters);
 }
